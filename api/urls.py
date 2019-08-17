0bac3cd05c5987937febb7cf880e531aea277c1d
slavov <kostadin.slavov@tixi.bg>
2019-08-17 12:49:16 +0300

    qr-code generator works

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
 from rest_framework.authtoken import views as auth_token_views
-
+from . import views
 urlpatterns = [
     path('users/', include('users.urls')),
     path('token-auth/', auth_token_views.obtain_auth_token, name='api-token-auth'),
+    path('generate-qr/', views.generate_qr )
     ]
--- api/views.py
+++ api/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.conf import settings
+import datetime
+import qrcode
+
 
 # Create your views here.
+
+
+
+def generate_qr(request):
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_H,
+        box_size=10,
+        border=4,
+    )
+
+    data = request.GET['qr_data']
+
+
+    qr.add_data(data)
+    qr.make(fit=True)
+    img = qr.make_image()
+    filename = "{}{}-{}{}".format(settings.MEDIA_ROOT+'transactions/qr_codes/',int(datetime.datetime.now().timestamp()), data, '.png')
+    img.save(filename)
+    return JsonResponse({'qr_code':'/media/'+filename.split('/media/')[1]})
+
