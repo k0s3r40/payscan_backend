e9bc64a09502d666369558604b5300e1c9b72f65
slavov <kostadin.slavov@tixi.bg>
2019-08-17 10:18:01 +0300

    Create the Wallet model

--- users/models.py
+++ users/models.py
@@ -6,3 +6,8 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     first_name = models.CharField(max_length=255, null=True, blank=True)
     last_name = models.CharField(max_length=255, null=True, blank=True)
+
+
+class Wallet(models.Model):
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    amount = models.DecimalField(decimal_places=2, max_digits=10)
